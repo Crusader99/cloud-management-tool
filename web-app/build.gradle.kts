29a3bd0bf13e06dad391906581c056542e90b484
Simon Forschner <26634807+Crusader99@users.noreply.github.com>
2021-04-24 15:16:14 +0200

    Configure muirwik and kotlinmaterialui libraries for react frontend

--- web-app/build.gradle.kts
+++ web-app/build.gradle.kts
@@ -2,13 +2,27 @@ plugins {
     kotlin("js")
 }
 
+repositories {
+    maven(url = "https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
+    maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-js-wrappers")
+    mavenCentral()
+}
+
 dependencies {
     implementation(project(":common"))
 
     // React components
-    implementation("org.jetbrains:kotlin-react:17.0.1-pre.148-kotlin-1.4.30")
-    implementation("org.jetbrains:kotlin-react-dom:17.0.1-pre.148-kotlin-1.4.30")
+    implementation("org.jetbrains:kotlin-react:17.0.2-pre.153-kotlin-1.4.32")
+    implementation("org.jetbrains:kotlin-react-dom:17.0.2-pre.153-kotlin-1.4.32")
     implementation("org.jetbrains:kotlin-styled:5.2.1-pre.148-kotlin-1.4.30")
+
+    implementation(npm("react-toolbox", "2.0.0-beta.13"))
+    implementation(npm("react-youtube-lite", "1.0.1"))
+
+    implementation("com.ccfraser.muirwik:muirwik-components:0.6.3")
+
+    implementation("net.subroh0508.kotlinmaterialui:core:0.5.6")
+    implementation("net.subroh0508.kotlinmaterialui:lab:0.5.6")
 
     // Test framework on javascript platform
     testImplementation(kotlin("test-js"))
@@ -16,6 +30,7 @@ dependencies {
 
 kotlin {
     js {
+        useCommonJs()
         binaries.executable()
         browser {
             // For continuous integration: gradle browserDevelopmentRun --continuous
@@ -27,31 +42,4 @@ kotlin {
             }
         }
     }
-}
-
-// Create index.html file for testing
-val publish by tasks.registering {
-    doLast {
-        val outputFolder = File(buildDir, "artifact-js")
-        val jsFile = File(outputFolder, project.name + ".js")
-        val htmlFile = File(buildDir, "index.html")
-        if (htmlFile.exists()) {
-            return@doLast // Skip
-        }
-        val text = StringBuilder()
-        text.append("<!DOCTYPE HTML><html lang=\"de\">")
-        text.append("<head>")
-        text.append("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/>")
-        text.append("</head>")
-        text.append("<body>")
-        text.append("<script type=\"text/javascript\" src=\"${outputFolder.name}/${jsFile.name}\"></script>")
-        text.append("</body>")
-        text.append("</html>")
-        htmlFile.writeText(text.toString(), Charsets.UTF_8)
-        println("Created html file at $htmlFile")
-    }
-}
-
-tasks.build {
-    finalizedBy(publish)
 }
