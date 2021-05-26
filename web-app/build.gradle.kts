905f876e869248b293808f5b22f53a1e876eb36c
Simon Forschner <26634807+Crusader99@users.noreply.github.com>
2021-05-26 21:31:35 +0200

    Gradle: Simplify declaration of kotlin wrapper modules

--- web-app/build.gradle.kts
+++ web-app/build.gradle.kts
@@ -8,13 +8,17 @@ repositories {
     mavenCentral()
 }
 
+// Simplify declaration of kotlin wrapper modules
+fun kotlinWrapper(module: String) = "org.jetbrains:kotlin-$module-pre.153-kotlin-1.4.32"
+
 dependencies {
     implementation(project(":common"))
 
     // React components
-    implementation("org.jetbrains:kotlin-react:17.0.2-pre.153-kotlin-1.4.32")
-    implementation("org.jetbrains:kotlin-react-dom:17.0.2-pre.153-kotlin-1.4.32")
-    implementation("org.jetbrains:kotlin-styled:5.2.1-pre.148-kotlin-1.4.30")
+    implementation(kotlinWrapper("react:17.0.2"))
+    implementation(kotlinWrapper("react-dom:17.0.2"))
+    implementation(kotlinWrapper("react-table:7.6.3")) // new: 7.7.0
+    implementation(kotlinWrapper("styled:5.2.3")) // new: 5.3.0
 
     implementation("com.ccfraser.muirwik:muirwik-components:0.6.7")
 
@@ -38,7 +42,8 @@ kotlin {
                 cssSupport.enabled = true
             }
             runTask {
-                devServer = devServer?.copy(port = 8081, proxy = mapOf("/api" to "http://localhost:80"))
+                val proxyTable = mapOf("/api" to "http://localhost:80")
+                devServer = devServer?.copy(port = 8081, proxy = proxyTable)
             }
         }
     }
