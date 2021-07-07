6ac7e27a90289e98f2ff180e545c2f20db527f39
Simon Forschner <26634807+Crusader99@users.noreply.github.com>
2021-07-07 15:30:48 +0200

    Update API doc for crypto function (#48)

--- common/src/commonMain/kotlin/de/hsaalen/cmt/crypto/HashFunctions.kt
+++ common/src/commonMain/kotlin/de/hsaalen/cmt/crypto/HashFunctions.kt
@@ -3,7 +3,8 @@ package de.hsaalen.cmt.crypto
 import com.soywiz.krypto.SHA256
 
 /**
- * Hash salted password and convert to hex string.
+ * Hash raw input string using sha256 and convert it to hex string.
+ * Note that salting needs to happen before this method call.
  */
 fun hashSHA256(input: String): String {
     return SHA256.digest(input.encodeToByteArray()).hex
